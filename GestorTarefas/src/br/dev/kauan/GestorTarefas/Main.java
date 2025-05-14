e515aeb84d2c0997b9d14e7ae3671297d59e162b
KAUAN LOPES PEREIRA <25132925@dsn127m1246727.cfp127aluno.local>
2025-05-14 11:30:06 -0300

    adicionado conteudo da aula

---
+++ GestorTarefas/src/br/dev/kauan/GestorTarefas/Main.java
@@ -0,0 +1,49 @@
+package br.dev.kauan.GestorTarefas;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String so = System.getProperty("os.name");
+		System.out.println(so);
+		
+		String caminho = "/Users/25132925/projetoGestorTarefas/Tarefa";
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		
+		try {
+			fr = new FileReader(caminho);
+			br = new BufferedReader(fr, 1);
+			
+			fw = new FileWriter(caminho, true);
+			bw = new BufferedWriter(fw);
+			
+			bw.append("Kauan\n");
+			bw.flush();
+			
+			String linha = br.readLine();
+			
+			while (linha != null) {
+				System.out.println(linha);
+				linha = br.readLine();
+			}
+			
+		} catch (FileNotFoundException erro) {
+			System.out.println("Arquivo não encontrado!");
+		} catch (IOException erro) {
+			System.out.println("Acesso negado!");
+		} catch (Exception erro) {
+			System.out.println(erro.getMessage());
+		}
+	}
+
+}
